7a2cf6517a90e83f10b24dcc6e1b2e4633c3a4bf
Paulo Varela <paulo.varela@escolar.ifrn.edu.br>
2024-11-28 16:18:28 -0300

    Criou o repositorio e salvando dados no banco

--- src/main/java/ifrn/pi/papum01/controllers/ProdutoControllers.java
+++ src/main/java/ifrn/pi/papum01/controllers/ProdutoControllers.java
@@ -1,17 +1,32 @@
 package ifrn.pi.papum01.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ifrn.pi.papum01.models.Produto;
+import ifrn.pi.papum01.repository.ProdutoRepository;
+
 @Controller
 public class ProdutoControllers {
+	
+	
+	
+	@Autowired // auto vincular para criar objeto
+	private ProdutoRepository pr;
+	
+	
 	@RequestMapping("/cadastro/produto")
 	public String CadastrarProduto() {
 		return "CadastrarProdutos";
 	}
 	@PostMapping("/Produto")
-	public String adicionar() {
+	public String adicionar(Produto produto) {
+		
+		System.out.println(produto);
+		pr.save(produto);
+		
 		return "produto-adicionado";
 	}
 }
