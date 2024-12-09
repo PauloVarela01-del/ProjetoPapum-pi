f729aeddc4694128e546b7b9f0fdd10062444cff
Stella Faby de Araujo Camara <20211204010062@ifrn.local>
2024-12-09 15:14:17 -0300

    Listando Produto

--- src/main/java/ifrn/pi/papum01/controllers/ProdutoControllers.java
+++ src/main/java/ifrn/pi/papum01/controllers/ProdutoControllers.java
@@ -1,7 +1,10 @@
 package ifrn.pi.papum01.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,26 +14,37 @@ import ifrn.pi.papum01.repository.ProdutoRepository;
 
 @Controller
 public class ProdutoControllers {
-	
-	
-	
-	@Autowired 
+
+	@Autowired
 	private ProdutoRepository pr;
-	
-	
+
 	@RequestMapping("/cadastro/produto")
 	public String CadastrarProduto() {
 		return "CadastrarProdutos";
 	}
-	@PostMapping("/Produto")
-	public String adicionar(Produto produto) {
-		
-		System.out.println(produto);
+
+	// @PostMapping("/Produto") //Produto
+	// public String adicionar(Produto produto) {
+
+	 
+	// pr.save(produto);
+
+	/// return "produto-adicionado";
+	// }
+
+	@PostMapping("/Produto") // Responsavel por Salvar e exibir os produtos
+	public ModelAndView listar(Produto produto) {
+	    System.out.println(produto);
 		pr.save(produto);
-		
-		return "produto-adicionado";
+		List<Produto> produtos = pr.findAll();
+		ModelAndView mv = new ModelAndView("lista");
+		mv.addObject("produtos", produtos);
+		return mv;
+
+		// produtos/lista
+
 	}
 	
+	
 
-   
 }
