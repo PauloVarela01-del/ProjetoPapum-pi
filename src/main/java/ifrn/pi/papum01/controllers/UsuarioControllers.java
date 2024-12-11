a19b35913adafbc600b8fa5ddc343c1200d5ef72
Paulo Varela <paulo.varela@escolar.ifrn.edu.br>
2024-12-11 10:27:04 -0300

    Listando Usuarios

--- src/main/java/ifrn/pi/papum01/controllers/UsuarioControllers.java
+++ src/main/java/ifrn/pi/papum01/controllers/UsuarioControllers.java
@@ -1,10 +1,15 @@
 package ifrn.pi.papum01.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import ifrn.pi.papum01.models.Produto;
 import ifrn.pi.papum01.models.Usuario;
 import ifrn.pi.papum01.repository.UsuarioRepository;
 
@@ -27,4 +32,12 @@ public class UsuarioControllers {
     	ur.save(usuario);
     	return"usuarios/usuario-adicionado";
     }
+    @GetMapping("/usuarios/lista") // Responsavel por Salvar e exibir os produtos //post
+	public ModelAndView listar() {
+		List<Usuario> usuarios = ur.findAll();
+		ModelAndView mv = new ModelAndView("lista");
+		mv.addObject("Usuario", usuarios);
+		return mv;
+    }
+    
 }
